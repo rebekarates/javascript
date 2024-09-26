d47694bb481e84e8999ec97abb49d31048af1fc9
Rebeka Ratess <rebekarates4@gmail.com>
2024-09-25 22:42:39 -0300

    alteracões JS

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel= 60.1
+console.log(`A vlocidade do seu carro é ${vel}`)
+if(vel > 60){ //condição simples
+    console.log('Você ultrapassou a velocidade, Multado!')
+}
+console.log('Dirija sempre com o cinto de segurança')
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais= 'eua'
+console.log(`Você vive em ${pais}`)
+if(pais=='Brasil'){
+    console.log('Você é Brasileiro')
+}
+else{
+    console.log('VocÊ é mexicano? tá preso')
+}
